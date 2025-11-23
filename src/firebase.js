9cdc4177f19d065036da3d6d3a654a8f5462e805
ABHISHEK WANI <110009400+Abhishek8827@users.noreply.github.com>
2025-11-24 00:19:47 +0530

    Obscure Firebase API key and app ID
    Replaced sensitive API key and app ID with placeholders.

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,12 @@ import { getFirestore, serverTimestamp } from "firebase/firestore";
 import { getStorage, ref as storageRef } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBZHtlpel5zdBEz8PMzSA2Aqp0szpfqnfw",
+  apiKey: "API KEY",
   authDomain: "drive-76237.firebaseapp.com",
   projectId: "drive-76237",
   storageBucket: "drive-76237.firebasestorage.app",
   messagingSenderId: "416031778110",
-  appId: "1:416031778110:web:416f0a30cb73a6eeb9b86f",
+  appId: "API IDf",
 };
 
 const app = initializeApp(firebaseConfig);
